721dfc9727a1546f79822d2802b9ffe914848f6a
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-11-29 12:30:05 -0300

    fix: import error (2)

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
-from application import decision_tree_appservice as tree_appservice
-from application import k_algorithms_appservice as k_appservice
-from application import regression_appservice as reg_appservice
-from application import repository_appservice as repo_appservice
+from src.application import decision_tree_appservice as tree_app_service
+from src.application import k_algorithms_appservice as k_app_service
+from src.application import regression_appservice as reg_app_service
+from src.application import repository_appservice as repo_app_service
 
-repo_appservice.clear_temp()
+repo_app_service.clear_temp()
 
-df = repo_appservice.get_treated_dataset()
+df = repo_app_service.get_treated_dataset()
 
-tree = tree_appservice.decision_tree(df, open_file=True)
+tree = tree_app_service.decision_tree(df, open_file=True)
 tree.print_overview()
