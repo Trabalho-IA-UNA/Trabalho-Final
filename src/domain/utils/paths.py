b86dfa7f4974c9265da1db8738d61788d6bf0be6
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-11-29 13:51:56 -0300

    fix: project structure

--- src/domain/utils/paths.py
+++ src/domain/utils/paths.py
@@ -2,11 +2,12 @@ import os
 import platform
 
 platform_system = platform.system()
+current_directory = os.getcwd()
 
-DATA_PATH = f"{os.getcwd()}\\infrastructure\\data" if platform_system == 'Windows' else f"{os.getcwd()}/infrastructure/data"
+DATA_PATH = f"{current_directory}\\src\\infrastructure\\data" if platform_system == 'Windows' else f"{current_directory}/src/infrastructure/data"
 """The project relative path to the folder 'data' in infrastructure."""
 
-DATASET_PATH = f'{DATA_PATH}\\android_apps_metadata.csv' if platform_system == 'Windows' else f'{DATA_PATH}/android_apps_metadata.csv'
+DATASET_PATH = f'{DATA_PATH}\\src\\android_apps_metadata.csv' if platform_system == 'Windows' else f'{DATA_PATH}/src/android_apps_metadata.csv'
 """The project relative path to the original dataset CSV file."""
 
 TEMP_PATH = f'{DATA_PATH}\\temp' if platform_system == 'Windows' else f'{DATA_PATH}/temp'
